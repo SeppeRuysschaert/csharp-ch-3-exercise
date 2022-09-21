7f498b34c0e7f3acd88a851be7a2e031adba7287
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-09-21 13:57:18 +0200

    Implement Deck class

--- src/Domain/Deck.cs
+++ src/Domain/Deck.cs
@@ -13,19 +13,37 @@ public class Deck
     #region Constructors
     public Deck()
     {
-        throw new NotImplementedException();
+        _random = new Random();
+        _cards = new List<BlackJackCard>(52);
+
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            foreach (FaceValue faceValue in Enum.GetValues(typeof(FaceValue)))
+                _cards.Add(new BlackJackCard(suit, faceValue));
+
+        Shuffle();
     }
     #endregion
 
     #region Methods
     public BlackJackCard Draw()
     {
-        throw new NotImplementedException();
+        if (_cards.Count == 0)
+            throw new InvalidOperationException("Cannot draw card from empty deck");
+
+        var card = _cards[0];
+        _cards.RemoveAt(0);
+        return card;
     }
 
     private void Shuffle()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < _cards.Count * 3; i++)
+        {
+            int randomPosition = _random.Next(0, _cards.Count);
+            var card = _cards[randomPosition];
+            _cards.RemoveAt(randomPosition);
+            _cards.Add(card);
+        }
     }
     #endregion
 }
